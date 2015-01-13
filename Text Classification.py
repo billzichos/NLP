deb05824823ca86f3a7f2e0a223b46cffaf67860
billzichos <billzichos@hotmail.com>
2015-01-12 21:24:54 -0500

    get some thoughts on paper for the test classification

---
+++ Text Classification.py
@@ -0,0 +1,13 @@
+## Author: Bill Zichos
+
+## Step 1: Combine the text for each category
+## Step 2: Create distinct word lists for each category
+## Step 3: Tokenize the test text.
+## Step 4: Loop through each word in the tokenized text.
+## Step 5: Does the token exist in either of categories?
+## Step 6: Summarize the results.
+
+for word in text:
+    # if word in cat1 then 1 else 0
+    # if word in cat2 then -1 else 0
+    # add them
